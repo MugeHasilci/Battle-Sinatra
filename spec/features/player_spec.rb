44c072db742ac40fa98cbdc0e871ce9fc5d3ca59
muge <muge.hasilci@hotmail.com>
2019-03-09 03:57:51 +0100

    implementing hit points

--- spec/features/player_spec.rb
+++ spec/features/player_spec.rb
@@ -5,16 +5,28 @@ describe Player do
   subject(:mittens) { Player.new('Mittens')}
 
   describe '#name' do
-    it 'returns the name' do
-      expect(dave.name).to eq 'Dave'
+      it 'returns the name' do
+        expect(dave.name).to eq 'Dave'
+      end
     end
-  end
 
-  describe '#hit_points' do
-    it 'returns the hit hit_points' do
-      expect(dave.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    describe '#hit_points' do
+      it 'returns the hit points' do
+        expect(dave.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+      end
     end
-  end
 
+    describe '#attack' do
+      it 'damages the player' do
+        expect(mittens).to receive(:receive_damage)
+        dave.attack(mittens)
+      end
+    end
 
+    describe '#receive_damage' do
+      it 'reduces the player hit points' do
+        expect { dave.receive_damage }.to change { dave.hit_points }.by(-10)
+      end
+    end
+   
 end
