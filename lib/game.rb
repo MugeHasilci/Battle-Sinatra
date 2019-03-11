89eef51a4110164fee5197e4459f09df9499a332
muge <muge.hasilci@hotmail.com>
2019-03-11 11:56:38 +0200

    tests are passing now

--- lib/game.rb
+++ lib/game.rb
@@ -3,6 +3,9 @@ class Game
   def initialize(player_1, player_2)
     @players = [player_1, player_2]
   end
+  def attack(player)
+    player.receive_damage
+  end
 
   def player_1
     @players.first
